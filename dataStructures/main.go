2a1a0583cc6c3b1c4ea618b8d8b8353aa4194880
nordluma <maxnordlund.95@hotmail.com>
2023-08-28 23:10:26 +0300

    feat(dataStructures): doubly linked node

--- dataStructures/main.go
+++ dataStructures/main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	node := newNode(3)
+	node := newSinglyLinkedNode(3)
 	fmt.Println(node.Value)
 }
--- dataStructures/node.go
+++ dataStructures/node.go
@@ -5,6 +5,16 @@ type SinglyLinkedNode[T any] struct {
 	Next  *SinglyLinkedNode[T]
 }
 
-func newNode[T any](value T) *SinglyLinkedNode[T] {
+func newSinglyLinkedNode[T any](value T) *SinglyLinkedNode[T] {
 	return &SinglyLinkedNode[T]{Value: value, Next: nil}
+}
+
+type DoublyLinkedNode[T any] struct {
+	Value T
+	Next  *DoublyLinkedNode[T]
+	Prev  *DoublyLinkedNode[T]
+}
+
+func newDoublyLinkedNode[T any](value T) *DoublyLinkedNode[T] {
+	return &DoublyLinkedNode[T]{Value: value, Next: nil, Prev: nil}
 }
