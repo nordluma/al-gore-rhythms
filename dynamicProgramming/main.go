0f6805c0d12be3d8ad082d4f73ac92e4478a4eaf
nordluma <maxnordlund.95@hotmail.com>
2023-08-29 21:57:39 +0300

    refactor(dynamicProgramming): create a function to test the fibonacci

--- dynamicProgramming/main.go
+++ dynamicProgramming/main.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+func testFib(msg string, n int, fn func(int) int) {
+	fmt.Println(msg)
+	fmt.Println(fn(n))
+	fmt.Println("==================")
+}
+
 func main() {
-	// result := fibonacci(40)
-	result := memFibonacci(50)
-	fmt.Println(result)
+	testFib("Normal Fibonacci", 40, fibonacci)
+	testFib("Memoized Fibonacci", 40, memFibonacci)
 }
