2df4e1f8c4100716ca19dc2a7504897b30e3c91f
nordluma <maxnordlund.95@hotmail.com>
2023-08-29 21:33:17 +0300

    feat(dynamicProgramming): memoized fibonacci

--- dynamicProgramming/fibonacci.go
+++ dynamicProgramming/fibonacci.go
@@ -7,3 +7,20 @@ func fibonacci(n int) int {
 
 	return fibonacci(n-1) + fibonacci(n-2)
 }
+
+func _memFibonacci(n int, cache map[int]int) int {
+	if n == 1 || n == 0 {
+		return n
+	}
+
+	if _, ok := cache[n]; !ok {
+		cache[n] = _memFibonacci(n-1, cache) + _memFibonacci(n-2, cache)
+	}
+
+	return cache[n]
+}
+
+func memFibonacci(n int) int {
+	cache := make(map[int]int)
+	return _memFibonacci(n, cache)
+}
--- dynamicProgramming/main.go
+++ dynamicProgramming/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	result := fibonacci(40)
+	// result := fibonacci(40)
+	result := memFibonacci(50)
 	fmt.Println(result)
 }
